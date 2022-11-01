9bf76d52c6e48f2ddc19a34fb06cd0971d1bf04d
asahi20215 <muzinoasahi@gmail.com>
2022-11-01 16:45:09 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const container=document.querySelector(".container");
-console.log(container);
-for(let index=0;index<=30;index++){
-    const containerE1=document.createElement("div");
-    containerE1.classList.add("containerdiv");
-    container.appendChild(containerE1);
+const containerE=document.querySelector(".containerBox");
+for (let index = 0; index < 29; index++) {
+    const containerEle=document.createElement("div");
+    containerEle.classList.add("container");
+    containerE.appendChild(containerEle);
+}
+
+const constainers=document.querySelectorAll(".container");
+assignCode();
+function assignCode(){
+    constainers.forEach((constainers)=>{
+        const newColorCode=colorGenerator();
+        constainers.style.backgroundColor="#"+newColorCode;
+        constainers.innerText="#"+newColorCode;
+    })
+}
+
+function colorGenerator(){
+    const code="0123456789abcdef";
+    const colorCodeLength=6;
+    let colorCode="";
+    for (let i = 0; i < colorCodeLength; i++) {
+        const colorIndex=Math.floor(Math.random()*code.length);
+        colorCode+= code.substring(colorIndex,colorIndex+1) 
+    }
+    return colorCode;
 }
